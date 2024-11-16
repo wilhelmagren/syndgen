1ee8175070da759d07543c8f84e1e9b2854515dc
wilhelmagren <wilhelmagren98@gmail.com>
2024-11-16 15:38:20 +0100

    feat: base metadata class

---
+++ syndgen/metadata/metadata.py
@@ -0,0 +1,59 @@
+"""
+MIT License
+
+Copyright (c) 2024 Wilhelm Ågren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+File created: 2024-11-16
+Last updated: 2024-11-16
+"""
+
+from __future__ import annotations
+
+from typing import (
+    Any,
+    Dict,
+)
+
+
+class Metadata:
+    """"""
+
+    def __init__(self: Metadata) -> None:
+        """"""
+        pass
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> Metadata:
+        """Create a new ``Metadata`` instance from a dictionary.
+
+        Parameters
+        ----------
+        d : dict
+            The dictionary containing all key-value pairs of the metadata.
+
+        Returns
+        -------
+        Metadata
+            The new instance of the ``Metadata`` class.
+        
+        """
+
+        return cls()
