862ef08d09e9a02078bae0fb5926681679f98fc4
wilhelmagren <wilhelmagren98@gmail.com>
2024-11-17 17:34:17 +0100

    feat: start work on tabular data transformer

---
+++ syndgen/processing/transformer.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import pandas as pd
+
+
+class Transformer:
+    """"""
+
+    def __init__(
+        self,
+        *,
+        max_bgm_clusters: int = 10,
+        weight_threshold: float = 5e-3,
+    ) -> None:
+        """"""
+
+        self._max_bgm_clusters = max_bgm_clusters
+        self._weight_threshold = weight_threshold
+    
+    def fit(
+        self,
+        data: pd.DataFrame,
+    ) -> Transformer:
+        """"""
+
+        n_output_dims = 0
+        column_info = {"activation": [], "transform": []}
+
+        if len(data) <= self._max_bgm_clusters:
+            self._max_bgm_clusters = max(len(data) - 1, 1)
+        
+        for column in data.columns:
+            pass
+
+        self._fitted = True
