0ef5d2063c0ea6ce564ea296e9012adb4390f593
wilhelmagren <wilhelmagren98@gmail.com>
2024-11-18 05:41:29 +0100

    feat: dtype transformer

--- syndgen/processing/transformer.py
+++ syndgen/processing/transformer.py
@@ -2,10 +2,15 @@ from __future__ import annotations
 
 import pandas as pd
 
-from typing import Any
+from syndgen.metadata import Metadata
+from typing import (
+    Any,
+    Iterable,
+    Optional,
+)
 
 
-class Transformer:
+class Transform:
     """"""
 
     def __init__(
@@ -22,7 +27,7 @@ class Transformer:
     def fit(
         self,
         data: pd.DataFrame,
-    ) -> Transformer:
+    ) -> Transform:
         """"""
 
         n_output_dims = 0
@@ -40,8 +45,9 @@ class Transformer:
 class IdentityTransform:
     """"""
 
-    def fit(self, *args, **kwargs) -> None:
+    def fit(self, *args, **kwargs) -> IdentityTransform:
         """"""
+        return self
 
     def transform(self, data: Any, *args, **kwargs) -> Any:
         """"""
@@ -50,3 +56,44 @@ class IdentityTransform:
     def inverse_transform(self, data: Any, *args, **kwargs) -> Any:
         """"""
         return data
+
+
+class DTypeTransform:
+    """"""
+
+    def fit(
+        self,
+        data: pd.DataFrame,
+        metadata: Metadata,
+        *,
+        discrete_columns: Optional[Iterable[str]] = None,
+        continuous_columns: Optional[Iterable[str]] = None,
+    ) -> None:
+        """"""
+
+        if discrete_columns is None:
+            discrete_columns = metadata.detect_discrete_columns()
+        
+        if continuous_columns is None:
+            continuous_columns = metadata.detect_continuous_columns()
+
+        col_metadata = metadata.columns
+        
+        col_dtype_info = []
+        for disc_col in discrete_columns:
+            col_dtype_info.append(self._fit_discrete(data[disc_col], col_metadata[disc_col], disc_col))
+        
+        self._fitted = True
+        self._col_dtype_info = col_dtype_info
+    
+    def _fit_discrete(
+        self,
+        data: pd.DataFrame,
+        col_metadata: Metadata,
+        disc_col: str,
+    ) -> None:
+        """"""
+
+        dtype = col_metadata.get("dtype", None)
+
+        
